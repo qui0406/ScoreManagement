38cf0d3b168021e09a0ad0528de6b98312e24f2c
qui0406 <anhqui04062004@gmail.com>
2025-08-03 22:29:42 +0700

    add subject

---
+++ ScoreManagement/src/main/java/com/scm/controllers/SocketHandler.java
@@ -0,0 +1,19 @@
+//package com.scm.controllers;
+//
+//import com.corundumstudio.socketio.SocketIOServer;
+//import lombok.AllArgsConstructor;
+//import lombok.NoArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Slf4j
+//public class SocketHandler {
+//    @Autowired
+//    private SocketIOServer socketIOServer;
+//
+//
+//}
